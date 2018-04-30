b7743fbf8e2615db752c89a9cb2cc231edcca15f
Samuel xu <18611768300@163.com>
2018-04-30 16:14:02 +0800

    支持http2 h2c upgrade

--- multiplexing-examples/jetty-client/src/main/java/de/consol/labs/h2c/examples/client/jetty/JettyClientExample.java
+++ multiplexing-examples/jetty-client/src/main/java/de/consol/labs/h2c/examples/client/jetty/JettyClientExample.java
@@ -8,33 +8,47 @@ import org.eclipse.jetty.http2.api.Session;
 import org.eclipse.jetty.http2.api.Stream;
 import org.eclipse.jetty.http2.api.server.ServerSessionListener;
 import org.eclipse.jetty.http2.client.HTTP2Client;
+import org.eclipse.jetty.http2.client.http.HttpClientTransportOverHTTP2;
 import org.eclipse.jetty.http2.frames.DataFrame;
 import org.eclipse.jetty.http2.frames.HeadersFrame;
 import org.eclipse.jetty.util.Callback;
 import org.eclipse.jetty.util.FuturePromise;
 import org.eclipse.jetty.util.Jetty;
-import org.eclipse.jetty.util.Promise;
 import org.eclipse.jetty.util.ssl.SslContextFactory;
 
 import java.net.InetSocketAddress;
+import java.util.concurrent.Phaser;
 import java.util.concurrent.TimeUnit;
 
 // Copied from the JavaDoc for org.eclipse.jetty.http2.client.HTTP2Client
 public class JettyClientExample {
 
     public static void main(String[] args) throws Exception {
-        long startTime = System.nanoTime();
+
+        if(args.length <2) {
+            System.err.println("Usage:java -jar jetty-client.jar host port [alpn]");
+            return;
+        }
+        String protocal = "HTTP://";
+        String host = args[0];
+        int port = Integer.valueOf(args[1]);
+        boolean alpn = args.length >= 3 && args[2].equals("alpn");
+
 
         // Create and start HTTP2Client.
         HTTP2Client client = new HTTP2Client();
-        SslContextFactory sslContextFactory = new SslContextFactory(true);
-        client.addBean(sslContextFactory);
+        HttpClientTransportOverHTTP2 transport = new HttpClientTransportOverHTTP2(client);
+        SslContextFactory sslContextFactory = null;
+        transport.setUseALPN(alpn);
+        if (alpn) {
+            protocal = "HTTPS://";
+            sslContextFactory = new SslContextFactory(true);
+            client.addBean(sslContextFactory);
+        }
         client.start();
 
         // Connect to host.
-        String host = "localhost";
-        int port = 8443;
-
+        System.out.println("Connect to:"+protocal+host+":"+port);
         FuturePromise<Session> sessionPromise = new FuturePromise<>();
         client.connect(sslContextFactory, new InetSocketAddress(host, port), new ServerSessionListener.Adapter(), sessionPromise);
 
@@ -45,29 +59,38 @@ public class JettyClientExample {
         HttpFields requestFields = new HttpFields();
         requestFields.put("User-Agent", client.getClass().getName() + "/" + Jetty.VERSION);
         // Prepare the HTTP request object.
-        MetaData.Request request = new MetaData.Request("GET", new HttpURI("https://" + host + ":" + port + "/"), HttpVersion.HTTP_2, requestFields);
+        MetaData.Request request = new MetaData.Request("GET", new HttpURI(protocal + host + ":" + port + "/"), HttpVersion.HTTP_2, requestFields);
         // Create the HTTP/2 HEADERS frame representing the HTTP request.
         HeadersFrame headersFrame = new HeadersFrame(request, null, true);
+        final Phaser phaser = new Phaser(2);
 
         // Prepare the listener to receive the HTTP response frames.
-        Stream.Listener responseListener = new Stream.Listener.Adapter()
-        {
+        Stream.Listener responseListener = new Stream.Listener.Adapter() {
+
             @Override
-            public void onData(Stream stream, DataFrame frame, Callback callback)
-            {
+            public void onHeaders(Stream stream, HeadersFrame frame) {
+                System.out.print("onHeader:");
+                System.out.println(stream);
+                if (frame.isEndStream()) phaser.arrive();
+            }
+            @Override
+            public void onData(Stream stream, DataFrame frame, Callback callback) {
+                System.out.print("onData:");
+                System.out.println(stream);
                 byte[] bytes = new byte[frame.getData().remaining()];
                 frame.getData().get(bytes);
-                int duration = (int) TimeUnit.NANOSECONDS.toSeconds(System.nanoTime() - startTime);
-                System.out.println("After " + duration + " seconds: " + new String(bytes));
+                System.out.println("data: " + new String(bytes));
                 callback.succeeded();
+                if (frame.isEndStream()) phaser.arrive();
             }
         };
 
         session.newStream(headersFrame, new FuturePromise<>(), responseListener);
-        session.newStream(headersFrame, new FuturePromise<>(), responseListener);
-        session.newStream(headersFrame, new FuturePromise<>(), responseListener);
+//        session.newStream(headersFrame, new FuturePromise<>(), responseListener);
+//        session.newStream(headersFrame, new FuturePromise<>(), responseListener);
 
-        Thread.sleep(TimeUnit.SECONDS.toMillis(20));
+
+        phaser.awaitAdvanceInterruptibly(phaser.arrive(), 5, TimeUnit.SECONDS);
 
         client.stop();
     }
